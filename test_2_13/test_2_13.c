c63acdfc47edff956af6d5b63698ba392f4453f5
李彦墨 <1784237811@qq.com>
2022-02-13 23:37:30 +0800

    第三节课

---
+++ test_2_13/test_2_13.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int a = 100;
+//int main()
+//{
+//	int age = 20;
+//	double weight = 63;
+//	age = age + 1;
+//	weight = weight - 1;
+//	printf(" % d\n", age);
+//	printf("%lf\n", weight);
+//	int a = 10;
+//	//局部变量优先
+//	printf("%d\n", a);
+//	return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d%d", & a ,& b);
+	sum = a + b;
+	printf("%d\n", sum);
+
+	return 0;
+}
