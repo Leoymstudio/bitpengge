3b2a486efb646fbf31ff618250e6f10d91ca4491
李彦墨 <1784237811@qq.com>
2022-03-14 00:04:30 +0800

    第十节课

---
+++ test_3_13/test_3_13.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", ~a);
+//	//~按位取反
+//		return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = ++a;//前置++,先++，后使用
+//	printf("%d\n",a);//11
+//	printf("%d\n",b);//11
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = a++;//后置++，先使用，后++
+//	printf("%d\n", a);//11
+//	printf("%d\n", b);//10
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int b = (++a)+ (++a)+ (++a);
+//	printf("%d\n", a);//4
+//	printf("%d\n", b);//12
+//	return 0;
+//}
+
+//int main()
+//{
+//	float a = 3.1415926525;
+//	printf("%f\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 0;
+//	int c = a && b;//并
+//	int d = a || b;//或
+//	printf("%d\n", c);
+//	printf("%d\n", d);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 0;
+//	int max = 0;
+//    max=a > b ? a : b;
+//    //相当于
+//    //if (a > b)
+//    //max = a;
+//    //else
+//    //max = b;
+//    printf("%d\n", max);
+//     return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 3;
+//	int c = 5;
+//	int d = (a = b - 2, c = a - 4, b = c + 2);
+//	//逗号表达式，从左向右计算，整个结果是最右边的结果
+//	printf("%d\n", d);
+//	return 0;
+//}
+
+
+//下标引用操作符[]
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", arr[9]);
+//}
+
+
+//函数调用操作符()
+int main()
+{
+	printf("%d\n", 10);
+}
+
+
